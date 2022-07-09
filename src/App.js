362af9f9101ca1d5a34012ce68a5ba8ceba7c929
Hossam Tuwano <hossamtuwano@gmail.com>
2022-07-10 01:09:24 +0300

    configured routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  HomePage,
+  News,
+  CryptoDetails,
+  CryptoCurrencies,
+  Exchanges,
+} from "./components";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/news  " element={<News />} />
+        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+        <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
       </Routes>
     </>
   );
